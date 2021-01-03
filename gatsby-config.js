1accf0552ab4caee2c0585a92122f84d87828744
Terry Brown <terence.r.brown@gsk.com>
2021-01-03 09:50:02 +0000

    Switching analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,17 +30,29 @@ module.exports = {
             }
         },
         {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-              // The property ID; the tracking code won't be generated without it
-              trackingId: "G-95EEG9TVCP",
-              // Setting this parameter is optional
-              anonymize: true,
-              // Setting this parameter is also optional
-              respectDNT: true,
-              // Defers execution of google analytics script after page load
-              defer: false,
+          resolve: `gatsby-plugin-google-gtag`,
+          options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+                "G-95EEG9TVCP", // Google Analytics / GA
+                //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+                //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+            ],
+            // This object gets passed directly to the gtag config command
+            // This config will be shared across all trackingIds
+            gtagConfig: {
+                optimize_id: "OPT-PQX5NQJ",
+                anonymize_ip: true,
+                cookie_expires: 0,
             },
-          }
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+            },
+          },
+        },
     ]
 };
